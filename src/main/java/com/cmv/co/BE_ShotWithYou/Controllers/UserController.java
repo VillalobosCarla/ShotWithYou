4468b2e37f3db316a3445bc08ebaef392f25a5f2
VillalobosCarla <carlavillalobos00@gmail.com>
2024-06-20 15:40:36 +0800

    configure to database

--- src/main/java/com/cmv/co/BE_ShotWithYou/Controllers/UserController.java
+++ src/main/java/com/cmv/co/BE_ShotWithYou/Controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.cmv.co.BE_ShotWithYou.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cmv.co.BE_ShotWithYou.Model.UserModel;
@@ -8,9 +12,27 @@ import com.cmv.co.BE_ShotWithYou.Model.UserModel;
 @RestController
 public class UserController {
     
+    //fetch one data
     //http.localhost:8080/user
     @GetMapping("/user")
     public UserModel getUser(){
         return new UserModel(1, "jeven 11","jeven.11@email.com", "jeven11");
+    }
+
+    //fetch multiple data
+    //http:localhost:8080/users
+    @GetMapping("/users")
+    public List<UserModel> getUsers(){
+        List<UserModel> users = new ArrayList<>();
+        users.add(new UserModel(1, "Camille", "camille@email.com", "camille1995"));
+        users.add(new UserModel(2, "Clarisse", "clarisse@email.com", "clarisse1997"));
+        users.add(new UserModel(3, "Carla", "carla@email.com", "carla2003"));
+        return users;
+    }
+
+    //http://localhost:8080/user/lalabsko
+    @GetMapping("/user/{name}")
+    public UserModel getUserFromName(@PathVariable("name")String name){
+        return new UserModel(1, "Lalabsko", "lalabsko@email.com", "lalabsko143");
     }
 }
