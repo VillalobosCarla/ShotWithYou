a969ff3f964ea63c462318d561403498bba9144c
VillalobosCarla <carlavillalobos00@gmail.com>
2024-06-26 16:31:01 +0800

    Final Activity_1

---
+++ src/main/java/com/cmv/co/BE_ShotWithYou/ExceptionHandler/ProductExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.cmv.co.BE_ShotWithYou.ExceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.cmv.co.BE_ShotWithYou.NotFoundException.ProductNotFoundException;
+
+@RestControllerAdvice
+public class ProductExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String productNotFoundHandler(ProductNotFoundException e){
+        return e.getMessage();
+    }
+}
